5b722196453b9c0de6d767aa6f84601b4da32f3a
Juan Rivarola <juanrivarola35@gmail.com>
2023-09-14 23:49:56 -0300

    boton eliminar

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ function refrescar() {
     });
 }
 
+function eliminarProducto(producto) {
+    const indice = productos.indexOf(producto);
+    if (indice !== -1) {
+        productos.splice(indice, 1); 
+        refrescar(); 
+    }
+}
 
 
 
@@ -59,8 +66,20 @@ function agregarProductoALista(producto) {
     precioSpan.textContent = `$${producto.precio}`;
     precioSpan.classList.add("precio-producto");
 
+    const botonEliminar = document.createElement("button");
+    botonEliminar.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+    </svg>
+    `;
+    botonEliminar.classList.add("boton-eliminar");
+    botonEliminar.addEventListener("click", () => {
+        eliminarProducto(producto); 
+    });
+
     li.appendChild(nombreSpan);
     li.appendChild(precioSpan);
+    li.appendChild(botonEliminar); 
 
     return li;
 }
